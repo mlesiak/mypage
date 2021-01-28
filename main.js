04ab583619399c8c871dcfa33fe1e461c8660ef9
Michał Lesiak <m.lesiaq@gmail.com>
2021-01-28 23:06:29 +0100

    media queries and hamburger menu added to website

--- main.js
+++ main.js
@@ -27,17 +27,6 @@ greet(age, name);
 
 // console.log(myResult);
 
-function createContent(querySelectorContent, content) {
-  const element = document.querySelector(querySelectorContent);
-
-  element.innerHTML = content;
-}
-
-createContent(
-  ".main__header--js",
-  "Ten nagłówek wypełniłem funkcją z innerHTML w JavaScript"
-);
-
 // const greet = (age, firstName) => {
 //     console.log(`Witaj odwiedzający, nazywam się ${firstName} i mam ${age} lat`)
 
@@ -45,42 +34,59 @@ createContent(
 
 // // greet(29, `Michał`);
 
-const humanOne = {
-  name: "Maciek",
-  age: 32,
-};
+// const humanOne = {
+//   name: "Maciek",
+//   age: 32,
+// };
 
-const humanTwo = {
-  name: "Stefan",
-  age: humanOne.age,
-};
+// const humanTwo = {
+//   name: "Stefan",
+//   age: humanOne.age,
+// };
 
-humanOne.age = 32;
+// humanOne.age = 32;
 
-console.log(humanTwo);
-console.log(humanOne);
+// console.log(humanTwo);
+// console.log(humanOne);
 
-if ("Java" != "Javascript") {
-  console.log("Java to nie Javascript");
-}
+// if ("Java" != "Javascript") {
+//   console.log("Java to nie Javascript");
+// }
 
-if (humanOne.age < humanTwo.age) {
-  console.log("HumanOne jest młodszy!");
-} else if (humanOne.age === humanTwo.age) {
-  console.log("Są równolatkami!");
-} else {
-  console.log("HumanOne jest starszy!");
-}
+// if (humanOne.age < humanTwo.age) {
+//   console.log("HumanOne jest młodszy!");
+// } else if (humanOne.age === humanTwo.age) {
+//   console.log("Są równolatkami!");
+// } else {
+//   console.log("HumanOne jest starszy!");
+// }
 
-const myNubmer = 3;
+// const myNubmer = 3;
 
-switch (myNubmer) {
-  case 7:
-    console.log("Jestem siódemką");
-    break;
-  case 9:
-    console.log("Jestem dziewiątką");
-    break;
-  default:
-    console.log("Jestem czymś innym!");
-}
+// switch (myNubmer) {
+//   case 7:
+//     console.log("Jestem siódemką");
+//     break;
+//   case 9:
+//     console.log("Jestem dziewiątką");
+//     break;
+//   default:
+//     console.log("Jestem czymś innym!");
+// }
+
+
+const button = document.querySelector('.action--js');
+
+button.addEventListener('click', () => {
+  const heading = document.querySelector('.main__header--js');
+  heading.innerHTML = 'Wypełniłem ten nagłówek za pomocą JS poprzez kliknięcie w przycisk';
+});
+
+const hamburger = document.querySelector('.hamburger--js');
+
+hamburger.addEventListener('click', () => {
+const nav = document.querySelector('.navigation--js');
+nav.classList.toggle('navigation--open');
+
+})
+
